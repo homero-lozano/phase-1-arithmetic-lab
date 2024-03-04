abdb10d6957f6506429ceac9f20b7b42ff0aa173
Nico Lozano <homeronicolaslozano@gmail.com>
2024-03-04 12:58:26 -0500

    Arthmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const multiply = 2 * 31
+let num1 = 2
+let num2 = 31
+const random = 1 
+1 < 0 
 
-// Write your code here
+let num3 = 16
+let num4 = 6
+const mod = num3 % num4
+const max = 20 
+Math.max(1,20)  
